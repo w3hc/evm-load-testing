2ca16ad93d866d7566019390a24c8d9d09372b12
julienbrg <julien@strat.cc>
2023-09-01 11:45:01 +0200

    add reset

---
+++ scripts/reset.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+import * as wallets from '../wallets.json'
+
+async function main() {
+
+  const initialWallets = {
+    "0": {
+        "address": "0xBAAe015b911929fA1Dd36961aA3871c9C36728E6",
+        "privateKey": "0x903a4d76a032eee960e48986ba303e21239897cdc542882b2ed4aa4b7c89c508"
+    }
+  }
+
+  
+  const jsonContent = JSON.stringify(initialWallets, null, 2);
+  fs.writeFileSync('wallets.json', jsonContent);
+  
+  console.log('Reset ✅');
+  
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
