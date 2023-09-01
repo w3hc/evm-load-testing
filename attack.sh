0d3e0b3624d843fd218cfe0d955bd94b0ccdcac9
julienbrg <julien@strat.cc>
2023-09-01 11:30:33 +0200

    reorder

--- attack.sh
+++ attack.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-pnpm now
-
 pnpm fund --num "$1" &&
 
-for ((i = 0; i < $1; i++)); do
+pnpm now &&
+
+for ((i = 0; i <= $1; i++)); do
   pnpm dance --wallet "$i" &
 done
 
+pnpm now &&
+
+echo "Thank you for spamming that network! ✅" &&
+
 wait
-
-pnpm now
-
-echo " "
-echo "Thank you for spamming that network! ✅"
